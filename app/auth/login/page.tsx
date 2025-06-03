2c5ab1f6895ba9a16cc89280375a7b6f308e136d
Mahendra Kumawat <mahendrakumawat80224@gmail.com>
2025-06-03 23:06:37 +0530

    update

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <div>
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <button>Login</button>
+  </div>
+  </div>
+  );
+}
