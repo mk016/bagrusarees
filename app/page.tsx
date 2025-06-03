05034b940339135adf544a61c9d4add619a6b8de
mk016 <mahendrakumawat80224@gmail.com>
2025-06-04 03:30:41 +0530

    Enhance Home Page Layout with Multiple Collections and Image Slider Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,35 @@
 import Image from 'next/image';
 import ImageSlider from '../Components/ImageSlider';
+import SuitCollection from '../Components/SuitCollection';
+import SareeCollection from '../Components/SareeCollection';
+import KotaDupattaSuitSets from '../Components/KotaDupattaSuitSets';
+import PopularMaheshwariSilkSuits from '../Components/PopularMaheshwariSilkSuits';
+import LinenSareeCollection from '../Components/LinenSareeCollection';
 
 export default function Home() {
   return (
     <main className="bg-white min-h-screen">
-      {/* Hero Section */}
-      <ImageSlider />
+  
+      {/* Image Slider Section */}
+      <section className="py-12 px-4">
+        <ImageSlider />
+      </section>
+
+      {/* Suit Collection */}
+      <SuitCollection />
+
+      {/* Saree Collection */}
+      <SareeCollection />
+
+      {/* Kota Dupatta Suit Sets */}
+      <KotaDupattaSuitSets />
+
+      {/* Popular Maheshwari Silk Suits */}
+      <PopularMaheshwariSilkSuits />
+
+      {/* Linen Saree Collection */}
+      <LinenSareeCollection />
+
       {/* Add more sections for Saree Collection, Testimonials, Store Info, etc. as needed */}
     </main>
   );
