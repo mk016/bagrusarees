85a64ef9c84c06d16b63035679d6c3a8e7f5ade6
mk016 <mahendrakumawat80224@gmail.com>
2025-06-05 21:23:03 +0530

    Add new saree and dupatta suit collection components to the Home page, enhancing the variety of showcased products.

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,19 @@ import SareeCollection from '../Components/SareeCollection';
 import KotaDupattaSuitSets from '../Components/KotaDupattaSuitSets';
 import PopularMaheshwariSilkSuits from '../Components/PopularMaheshwariSilkSuits';
 import LinenSareeCollection from '../Components/LinenSareeCollection';
+import KotaDoriaSareeCollection from '../Components/KotaDoriaSareeCollection';
+import MulmulDupattaSuitSets from '../Components/MulmulDupattaSuitSets';
+import ChiffonDupattaSuitSets from '../Components/ChiffonDupattaSuitSets';
+import MaheshwariSilkSareeCollection from '../Components/MaheshwariSilkSareeCollection';
+import ChanderiSilkSareeCollection from '../Components/ChanderiSilkSareeCollection';
+import CottonSareeCollection from '../Components/CottonSareeCollection';
 
 export default function Home() {
   return (
     <main className="bg-white min-h-screen">
-  
+      {/* Hero Section */}
+    
+
       {/* Image Slider Section */}
       <section className="py-12 px-4">
         <ImageSlider />
@@ -21,14 +29,22 @@ export default function Home() {
       {/* Saree Collection */}
       <SareeCollection />
 
-      {/* Kota Dupatta Suit Sets */}
+      {/* Kota Dupatta Suit Sets (from original image) */}
       <KotaDupattaSuitSets />
 
-      {/* Popular Maheshwari Silk Suits */}
+      {/* Popular Maheshwari Silk Suits (from original image) */}
       <PopularMaheshwariSilkSuits />
 
-      {/* Linen Saree Collection */}
+      {/* Linen Saree Collection (from original image) */}
       <LinenSareeCollection />
+
+      {/* New sections based on provided images */}
+      <KotaDoriaSareeCollection />
+      <MulmulDupattaSuitSets />
+      <ChiffonDupattaSuitSets />
+      <MaheshwariSilkSareeCollection />
+      <ChanderiSilkSareeCollection />
+      <CottonSareeCollection />
 
       {/* Add more sections for Saree Collection, Testimonials, Store Info, etc. as needed */}
     </main>
